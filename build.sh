524aab09160bef53658aff3d7a7299b721b4deec
Dominik Säume <dominik.saeume@hmmh.de>
2025-03-07 19:44:44 +0100

    Reproduction

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 source /quasi-msys2/env/all.src
-cargo build --release
+RUSTFLAGS='-C target-feature=+crt-static' cargo build --release
+cp /mingw64/bin/*.dll /app/target/x86_64-pc-windows-gnu/release/
